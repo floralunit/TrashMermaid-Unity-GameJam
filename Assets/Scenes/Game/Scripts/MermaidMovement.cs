d45627633c60df14dd57dd109373c30ced4ffe88
floralunit <71765648+floralunit@users.noreply.github.com>
2023-05-20 03:53:17 +0300

    сделано передвижение с анимациями и генерация объектов

---
+++ Assets/Scenes/Game/Scripts/MermaidMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MermaidMovement : MonoBehaviour
+{
+    public static MermaidMovement Instance;
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 movement;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
---
+++ Assets/Scenes/Game/Scripts/RandomGeneration.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGeneration2 : MonoBehaviour
+{
+    public int numberObject; // какое кол-во надо заспавнить
+    public GameObject[] objects;
+    private int generatedObject = 0;
+    public float minRange, maxRange; //территория спавна
+
+    void Update()
+    {
+        //считает сколько объектов заспавнено
+        if (generatedObject < numberObject)
+        {
+            Generate();
+            generatedObject++;
+        }
+
+    }
+    //сама генерация 
+    public void Generate()
+    {
+        int rand = Random.Range(0, objects.Length);
+        var cell = Instantiate(objects[rand], transform.position, Quaternion.identity);
+        cell.transform.position = new Vector3(Random.Range(minRange, maxRange), Random.Range(minRange, maxRange), transform.position.z);
+
+    }
+}
